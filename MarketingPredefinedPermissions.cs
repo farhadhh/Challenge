e9f1fa3c90185184469bfdfe99eea3d13f4c5cfc
farhadhh <parnian_farhad@yahoo.com>
2017-04-06 15:32:29 +0430

    ads
    ads

---
+++ MarketingPredefinedPermissions.cs
@@ -0,0 +1,10 @@
+﻿namespace VirtoCommerce.MarketingModule.Web.Security
+{
+    public static class MarketingPredefinedPermissions
+    {
+        public const string Read = "marketing:read",
+            Access = "marketing:access",
+            Update = "marketing:update",
+            Delete = "marketing:delete";
+    }
+}
